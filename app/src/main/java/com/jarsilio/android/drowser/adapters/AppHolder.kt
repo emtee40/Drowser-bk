7c5cbbf22e6a719b66bbebde8a7e1feff59b3709
Juan Garcia Basilio <juanitobananas@gmail.com>
2018-07-23 07:38:00 +0200

    Add a toast while tapping on cards that tells us if we added or removed an app from 'to drowse' list.
    Also use card views context so that we don't need to store a
    passthrough context.

--- app/src/main/java/com/jarsilio/android/drowser/adapters/AppHolder.kt
+++ app/src/main/java/com/jarsilio/android/drowser/adapters/AppHolder.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.jarsilio.android.drowser.R
 import com.jarsilio.android.drowser.models.AppInfo
 import com.jarsilio.android.drowser.models.DrowseCandidatesManager
@@ -15,7 +16,6 @@ class AppHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val appName = view.findViewById<View>(R.id.app_name) as TextView
     private val packageName = view.findViewById<View>(R.id.package_name) as TextView
     private val cardView = view.findViewById<View>(R.id.card_view) as CardView
-    private val passthroughContext = view.context
 
     fun updateWithAppItem(app: AppInfo) {
         icon.setImageDrawable(app.icon)
@@ -25,10 +25,16 @@ class AppHolder(view: View) : RecyclerView.ViewHolder(view) {
         // Adding click listener on CardView to open clicked application directly from here .
         cardView.setOnClickListener {
             Timber.d("Clicked on ${app.packageName}.")
-            val dm = DrowseCandidatesManager(passthroughContext)
+            val dm = DrowseCandidatesManager(cardView.context)
             if (dm.isDrowseCandidate(app.packageName)) {
+                Toast.makeText(cardView.context,
+                        cardView.context.getString(R.string.toast_remove_drowse, app.name),
+                        Toast.LENGTH_LONG).show()
                 dm.removeDrowseCandidate(app.packageName)
             } else {
+                Toast.makeText(cardView.context,
+                        cardView.context.getString(R.string.toast_add_drowse, app.name),
+                        Toast.LENGTH_LONG).show()
                 dm.addDrowseCandidate(app.packageName)
             }
         }
