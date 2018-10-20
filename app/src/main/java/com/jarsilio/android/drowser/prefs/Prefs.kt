06193add5f6a49ee6e8cdedbeec97322841b9763
Juan Garcia Basilio <juanitobananas@gmail.com>
2018-10-20 12:57:54 +0200

    prefs: Small refactor

--- app/src/main/java/com/jarsilio/android/drowser/prefs/Prefs.kt
+++ app/src/main/java/com/jarsilio/android/drowser/prefs/Prefs.kt
@@ -6,21 +6,24 @@ import android.preference.PreferenceManager
 import com.jarsilio.android.drowser.R
 
 class Prefs(context: Context) {
-    val IS_ENABLED = context.getString(R.string.pref_enabled_key)
-    val SHOW_NOTIFICATION = context.getString(R.string.pref_notification_key)
-    val REQUEST_ROOT_ACCESS = context.getString(R.string.pref_request_root_access_key)
-
     val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    /* User prefs */
+    val IS_ENABLED: String = context.getString(R.string.pref_enabled_key)
+    val SHOW_NOTIFICATION: String = context.getString(R.string.pref_notification_key)
 
     var isEnabled: Boolean
         get() = prefs.getBoolean(IS_ENABLED, false)
         set(value) = prefs.edit().putBoolean(IS_ENABLED, value).apply()
 
-    var requestRootAccess: Boolean
-        get() = prefs.getBoolean(REQUEST_ROOT_ACCESS, true)
-        set(value) = prefs.edit().putBoolean(REQUEST_ROOT_ACCESS, value).apply()
-
     var showNotification: Boolean
         get() = prefs.getBoolean(SHOW_NOTIFICATION, true)
         set(value) = prefs.edit().putBoolean(SHOW_NOTIFICATION, value).apply()
+
+    /* Automagic prefs: these are not real prefs. They are just set by the app to remember stuff */
+    val REQUEST_ROOT_ACCESS: String = context.getString(R.string.pref_request_root_access_key)
+
+    var requestRootAccess: Boolean
+        get() = prefs.getBoolean(REQUEST_ROOT_ACCESS, true)
+        set(value) = prefs.edit().putBoolean(REQUEST_ROOT_ACCESS, value).apply()
 }
