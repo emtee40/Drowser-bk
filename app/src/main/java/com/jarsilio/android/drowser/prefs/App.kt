223b8f244fbf53ecb3dd565643ae83f95aae3348
Juanito <juanitobananas@gmail.com>
2020-11-01 09:07:35 +0100

    Use libcommon's LongTagTree.

--- app/src/main/java/com/jarsilio/android/drowser/prefs/App.kt
+++ app/src/main/java/com/jarsilio/android/drowser/prefs/App.kt
@@ -1,54 +1,14 @@
 package com.jarsilio.android.drowser.prefs
 
 import android.app.Application
-import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
+import com.jarsilio.android.common.logging.LongTagTree
 import timber.log.Timber
-
-const val MAX_TAG_LENGTH = 23
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(LongTagTree(packageName))
+        Timber.plant(LongTagTree(applicationContext))
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-    }
-
-    inner class LongTagTree(private val packageName: String) : Timber.DebugTree() {
-        private fun getMessage(tag: String?, message: String): String {
-            val longTagTreeMessage: String
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
-                // Tag length limitation (<23): Use truncated package name and add class name to message
-                longTagTreeMessage = "$tag: $message"
-            } else {
-                // No tag length limit limitation: Use package name *and* class name
-                longTagTreeMessage = message
-            }
-            return longTagTreeMessage
-        }
-
-        private fun getTag(tag: String?): String {
-            var longTagTreeTag: String
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
-                // Tag length limitation (<23): Use truncated package name and add class name to message
-                longTagTreeTag = packageName
-                if (longTagTreeTag.length > MAX_TAG_LENGTH) {
-                    val shortPackageName = packageName.substring(packageName.length - MAX_TAG_LENGTH + 3, packageName.length)
-                    longTagTreeTag = "...$shortPackageName"
-                }
-            } else {
-                // No tag length limit limitation: Use package name *and* class name
-                longTagTreeTag = "$packageName $tag"
-            }
-
-            return longTagTreeTag
-        }
-
-        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-            val longTagTreeMessage = getMessage(tag, message)
-            val longTagTreeTag = getTag(tag)
-
-            super.log(priority, longTagTreeTag, longTagTreeMessage, t)
-        }
     }
 }
